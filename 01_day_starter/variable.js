6128c56b45337333bea5b64aeebb4f37f879fd5c
mrAkshatJoshi <mrakshatjoshi@gmail.com>
2022-06-02 05:20:13 +0530

    first

---
+++ 01_day_starter/variable.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+
+let firstName = 'Akshat'    // first name of a person
+let lastName = 'Joshi'      // last name of a person
+let country = 'INDIA'       // country
+let city = 'Bangalore'         // capital city
+let age = 24                 // age in years
+let isMarried = false
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Akshat JOSHI',     //name of a person
+  job = 'js Developer',
+  live = 'INDIA'
